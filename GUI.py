4bbefe881c1a8bd14725c3f49a682887534e6bac
royataya <roy_ataya@sfu.ca>
2020-04-06 14:47:43 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,11 +1,18 @@
 from tkinter import *
 
+master = Tk()
+
 def startGUI(canvasTitle, canvasWidth, canvasHeight):
-    master = Tk()
     master.title(canvasTitle)
     window = Canvas(master, width = canvasWidth, height = canvasHeight)
     window.pack()
 
+def closeButton():
+    button = Button(master, text = "End", width = 10, command=master.destroy)
+    button.pack()
+
+
 if __name__ == '__main__':
     startGUI('Window Title',640, 480)
+    closeButton()
     mainloop()
