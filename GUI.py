9c3e71f7340f8491840450135e60edf5fd870d5b
royataya <roy_ataya@sfu.ca>
2020-04-07 11:42:20 -0700

    update to GUI code

--- GUI.py
+++ GUI.py
@@ -1,18 +1,37 @@
 from tkinter import *
+import random
 
-master = Tk()
+puzzleOptionList = ["Select puzzle size", "8 - puzzle", "15 - puzzle"]
+algorithmOptionList = ["Select solving Algoritm","BFS", "DFS", "AStar"]
 
-def startGUI(canvasTitle, canvasWidth, canvasHeight):
+def startGUI(master, canvasTitle, canvasWidth, canvasHeight):
     master.title(canvasTitle)
+    Label(text = "Sliding puzzle", font = ("", 50)).pack()
+    Label(text = "choose size of the puzzle and the algorithm to solve it.", font = ("", 25)).pack()
     window = Canvas(master, width = canvasWidth, height = canvasHeight)
     window.pack()
 
-def closeButton():
-    button = Button(master, text = "End", width = 10, command=master.destroy)
-    button.pack()
+def choosePuzzle(master):
+    variable = StringVar(master)
+    variable.set(puzzleOptionList[0])
+    opt = OptionMenu(master, variable, *puzzleOptionList)
+    opt.config(font=('Helvetica', 15))
+    opt.pack(side = RIGHT)
 
+def chooseAlgorithm(master):
+    variable = StringVar(master)
+    variable.set(algorithmOptionList[0])
+    opt = OptionMenu(master, variable, *algorithmOptionList)
+    opt.config(font=('Helvetica', 15))
+    opt.pack(side = RIGHT)
+
+def randomize(master):
+    Button(master, text = "Randomize", command=NONE).pack(side = RIGHT)
 
 if __name__ == '__main__':
-    startGUI('Window Title',640, 480)
-    closeButton()
+    master = Tk()
+    startGUI(master, 'Sliding Puzzle', 680, 480)
+    choosePuzzle(master)
+    chooseAlgorithm(master)
+    randomize(master)
     mainloop()
