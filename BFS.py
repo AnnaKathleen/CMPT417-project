9363c64838145423b67af6f8e69870eb967bf872
royataya <roy_ataya@sfu.ca>
2020-04-13 16:34:31 -0700

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,4 +1,9 @@
 from node import Node
-def BFS(startState, goalState):
-    explored, queue = set(), de
+
+def BFS(startState, goalNode):
+    print("searching for solution with BFS\n")
+    actions = ["up","down","left","right"]
+    visited = []
+    
+
     pass
