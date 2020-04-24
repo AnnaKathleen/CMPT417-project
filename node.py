ada1a33d618db6f2f654b0feb3aab23dcbb09782
royataya <roy_ataya@sfu.ca>
2020-04-24 11:46:57 -0700

    updated Node.py
    added overloaded operators for equal and less than

--- node.py
+++ node.py
@@ -9,3 +9,11 @@ class Node:
 
         if self.node:
             self.map = ''.join(str(s) for s in self.node)
+
+    # overloaded the equal and less than operator to work with nodes 
+    # taken from https://www.geeksforgeeks.org/operator-overloading-in-python/
+    def __eq__(self, other):
+        return self.map == other.map
+
+    def __lt__(self, other):
+        return self.map < other.map
