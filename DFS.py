47430eb83ebbf2e2c26d5b367c87bd0381db1a2b
Anna Kathleen <37051025+AnnaKathleen@users.noreply.github.com>
2020-04-24 01:42:00 -0600

    Update DFS.py
    updating DFS to run individually, don't remove anything yet plz

--- DFS.py
+++ DFS.py
@@ -1,43 +1,54 @@
-from collections import deque
+from queue import LifoQueue
+import numpy as np
+import timeit
 
-class Node():
-	def __init__(self,parent,stateMat,x_loc,y_loc,cost,depth):
-		self.parent
-		self.stateMat
-		self.x_loc
-		self.y_loc
-		self.cost
-		self.depth
+num_nodes_expanded = 0
+puzzleSize = 0
+puzzle_side_len = 0
+
+class Node:
+	def __init__(self,stateMat,parent,action,x_loc,y_loc,cost,depth):
+		self.stateMat = stateMat
+		self.parent = parent
+		self.x_loc = x_loc
+		self.y_loc = y_loc
+		self.action = action
+		self.cost = cost
+		self.depth = depth
 
 		if self.stateMat:
 			self.map = ''.join(str(locs) for locs in self.stateMat)
 
-	def __eq__(self, other):
-        return self.map == other.map
 
-    def __lt__(self, other):
-        return self.map < other.map
-
+def matStacker(mat):
+	a = np.array(mat[0:3])
+	b = np.array(mat[3:6])
+	c = np.array(mat[6:10])
+	d = np.stack((a,b,c))
+	print("stacked array is: \n", d)
+	return d
 
 def findBlank(mat):
-	for i in range(0,N+1):
-		for j in range(0,N+1):
-			if(mat[i][j]==0):
-				return x,y
+	s = matStacker(mat)
+	for i in range(0,3):
+		for j in range(0,3):
+			if(s[i][j]==0):
+				return i,j
 
 def getSuccessors(currNode):
     global num_nodes_expanded
     num_nodes_expanded += 1
     children = list()
     for i in range(1,5):
-        newPosition = currNode.node[:]
+        newPosition = currNode.stateMat[:]
         index = newPosition.index(0)
         noneTrue = False
         if i == 1: # move the 0 up
             if index not in range(0, puzzle_side_len):
-                tempState = newPosition[index - puzzle_side_len]
-                newPosition[index - puzzle_side_len] = newPosition[index]
-                newPosition[index] = tempState
+            	#print("moving 0 up is out of range")
+            	tempState = newPosition[index - puzzle_side_len]
+            	newPosition[index - puzzle_side_len] = newPosition[index]
+            	newPosition[index] = tempState
             else:
                 noneTrue = True
                 finalPosition = None
@@ -68,13 +79,14 @@ def getSuccessors(currNode):
             else:
                 noneTrue = True
                 finalPosition = None
+        #might be mistake here
         if noneTrue == True:
-            children.append(Node(finalPosition,currNode,i,0))
+            children.append(Node(finalPosition,currNode,i,0,0,0,0))
         else:
-            children.append(Node(newPosition,currNode,i,0))
+            children.append(Node(newPosition,currNode,i,0,0,0,0))
             # print(children)
     
-    successors = [children for children in children if children.node]
+    successors = [children for children in children if children.stateMat]
     # print(successors)
     return successors
 
@@ -82,7 +94,7 @@ def getSuccessors(currNode):
 def stepBack(startNode,node):
     currNode = node
     directions = list()
-    while  currNode.node != startNode:
+    while  currNode.stateMat != startNode:
         if currNode.action == 1:
             currAction = 'move 0 Up'
         elif currNode.action == 2:
@@ -100,22 +112,22 @@ def DFS(initialState, goalTest):
 	
 	global goal_node
 	# max_search_depth, max_stack_size
-	openList = deque()
+	openList = LifoQueue()
 	closedList = set()
 	#this should be outside, before?
 	x_origin,y_origin = findBlank(initialState)
-	currNode = Node(None,initialState,x_origin,y_origin,0,0)
+	currNode = Node(initialState,None,x_origin,y_origin,0,0,0)
 	
-	openList.append(currNode)
+	openList.put(currNode)
 
 	# if time_elapsed < time_constraint:
 	# 	continue
 
 	while not openList.empty():
-		nodeNode = openList.pop()
+		nodeNode = openList.get()
 		closedList.add(nodeNode)
 
-		if nodeNode.stateMat == goalState:
+		if nodeNode.stateMat == goalTest:
 			goal_node = nodeNode
 			return openList
 
@@ -123,7 +135,7 @@ def DFS(initialState, goalTest):
 
 		for state in possibleStates:
 			if state.map not in closedList:
-				openList.append(state)
+				openList.put(state)
 				closedList.add(state.map)
 
 		# 		if state.depth > max_search_depth:
@@ -131,3 +143,47 @@ def DFS(initialState, goalTest):
 
 		# if len(openList) > max_stack_size:
 		# 	max_stack_size += 1
+
+def export(frontier, time):
+
+    #global moves
+
+    moves = stepBack(startState,goal_node)
+
+    file = open('output.txt', 'w')
+    file.write("path_to_goal: " + str(moves))
+    file.write("\ncost_of_path: " + str(len(moves)))
+    file.write("\nnum_nodes_expanded: " + str(num_nodes_expanded))
+    #file.write("\nfringe_size: " + str(len(frontier)))
+    #file.write("\nmax_fringe_size: " + str(max_frontier_size))
+    file.write("\nsearch_depth: " + str(goal_node.depth))
+    #file.write("\nmax_search_depth: " + str(max_search_depth))
+    file.write("\nrunning_time: " + format(time, '.8f'))
+    #file.write("\nmax_ram_usage: " + format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0, '.8f'))    
+    file.close()
+
+def main():
+    global puzzleSize, puzzle_side_len
+    global startState 
+    startState = [1,2,0,3,4,5,6,7,8]
+    goalState = [0,1,2,3,4,5,6,7,8]
+    puzzleSize = len(startState)
+    puzzle_side_len = int(puzzleSize ** 0.5)
+    res = DFS(startState, goalState)
+    
+    start = timeit.default_timer()
+    #frontier = function(initial_state)
+    res = DFS(startState, goalState)
+    stop = timeit.default_timer()
+
+    #export(res, stop-start)
+    print("result of DFS: ")
+    if res:
+    	export(res, stop-start)
+    	unstacked = goal_node.stateMat
+    	stacked = matStacker(unstacked)
+    	print("goal state found : \n", stacked)
+    else:
+    	print("no solution found :( ")	
+
+main()
